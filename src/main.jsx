e98e246380fbfd84ad1c919d025316ad547077a9
pataaaaaka <pataaaaaka@gmail.com>
2026-01-08 09:41:40 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import KeirakuBomber from './KeirakuBomber'
+import ResponsiveKeirakuBomber from './ResponsiveWrapper'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <KeirakuBomber />
+    <ResponsiveKeirakuBomber />
   </React.StrictMode>,
 )
+
